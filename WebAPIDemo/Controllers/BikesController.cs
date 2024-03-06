944ceaf2aa64ed08cf3d3aefeacbb9851fe27018
nahoshi002 <anhhaonguyen002@gmail.com>
2024-03-06 11:58:45 +0700

    Add project files.

---
+++ WebAPIDemo/Controllers/BikesController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BikesController : ControllerBase
+    {
+        [HttpGet]
+        public String GetBikes()
+        {
+            return "Trả về tất cả các xe";
+        }
+
+        [HttpGet("{id}")]
+        public String GetBikeByID(int id)
+        {
+            return $"Trả về xe có id: {id}";
+        }
+
+        [HttpPost]
+        public String CreatingBike()
+        {
+            return $"Thêm mới thông tin của xe";
+        }
+
+        [HttpPut("{id}")]
+        public String UpdateBike(int id)
+        {
+            return $"Thay đổi thông tin của xe có id: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public String DeleteBike(int id)
+        {
+            return $"Xóa thông tin của xe có id: {id}";
+        }
+    }
+}
---
+++ WebAPIDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+//Routing
+
+//"/bikes"
+
+
+
+app.Run();
+
